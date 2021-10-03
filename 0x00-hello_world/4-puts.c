e1a1275e7d7647276671a5240669ac25f199e34a
carolinarodg <drodriguezg2808@gmail.com>
2021-10-03 15:03:42 -0500

    [modify]_task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 /*
  *print with puts
  */
-int main(void){
-    puts("Programming is like building a multilingual puzzle");
-    return(0);
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
-
